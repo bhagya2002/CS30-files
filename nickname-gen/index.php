74dc35bf14ecf56632ff089e4278052a8a7e6b3d
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-04-11 18:48:31 -0600

    Update index.php

--- nickname-gen/index.php
+++ nickname-gen/index.php
@@ -22,7 +22,7 @@ $varall = ' ';
 // loops through the whole array and displys all the nicknames
  for ($i = 1 ;$i < count($nicknames)+1; $i++) {
    $indexer = $i -1;
-  $varall = $nicknames[$indexer];
+  $_POST['value-a'] = $nicknames[$indexer];
   echo $varall;
  }
 } 
@@ -60,7 +60,7 @@ $varall = ' ';
   <p id="fname-d"><?php echo "$fname"; ?></p>
   <!-- random nickname -->
   <p id="randname-d" name="value"><?php echo "$randval";?></p>
-  <p id="allname-d" name="value"><?php echo $varall . ',';?></p>
+  <p id="allname-d" name="value-a"><?php echo $varall . ',';?></p>
   <!-- last name -->
   <p id="last-d"><?php echo "$lname"; ?></p>
 </div>
