7e7c154a1c5d08998e3c201320b4dff117f310ef
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-04-11 19:40:18 -0600

    Update index.php

--- nickname-gen/index.php
+++ nickname-gen/index.php
@@ -16,6 +16,19 @@ if (isset($_GET['random'])) {
  $varall = " ";
 } 
 
+  // list all nicknames
+  if (isset($_GET['all'])) {
+    $indexer = 0;
+    
+    //  run this
+    // loops through the whole array and displys all the nicknames
+     for ($i = 1 ; $i < count($nicknames) +1; $i++) {
+        $indexer = $i -1;
+        $randval = " ";
+        $varall = "$nicknames[$indexer]<br>";
+     }
+    } 
+
 ?>
 
 <!DOCTYPE html>
@@ -55,19 +68,7 @@ if (isset($_GET['random'])) {
   ?></p>
   <p id="allname-d" name="value-a">
   <?php  
-  // list all nicknames
-if (isset($_GET['all'])) {
-  $indexer = 0;
-  
-  //  run this
-  // loops through the whole array and displys all the nicknames
-   for ($i = 1 ; $i < count($nicknames) +1; $i++) {
-      $indexer = $i -1;
-      $randval = " ";
-      $varall = "$nicknames[$indexer]<br>";
-  echo $varall;
-   }
-  } 
+ echo $varall;
   ?></p>
   <!-- last name -->
   <p id="last-d"><?php echo "$lname"; ?></p>
