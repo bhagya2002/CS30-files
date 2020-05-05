35b044ecd4c845fb723f192452eadff6b6397a0d
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-05-04 23:08:10 -0600

    sort algo's

---
+++ sort-analyzer-starter-master/js/search-sort.js
@@ -0,0 +1,49 @@
+// Library of Search & Sort Functions
+
+// Use Bubble Sort algorithm to sort array argument
+function bubbleSort(anArray) {
+    // loop through the whole array to compare
+    for (let i = 0; i < anArray.length; i++) {
+        // loop through it again for number of comparisions requred
+        for (let x = 0; x < anArray.length; x++) {
+            // if the second value (larger index) is smaller than the first one (smaller index) then...
+            if (anArray[x] > anArray[x + 1]) {
+                // set a temporary index at the value of the one on the left
+                let temp = anArray[x];
+                //  this performs the swap. The smaller value moves to the left in the array
+                anArray[x] = anArray[x + 1];
+                // new temperoary index is assigned (one up from the new smaller value index)
+                anArray[x + 1] = temp;
+            }
+        }
+    }
+}
+
+// Use Selection Sort algorithm to sort array argument
+function selectionSort(anArray) {
+    // loop through the whole array
+    for (let i = 0; i < anArray.length; i++) {
+        // set min position to the current fill slot
+        let min = i;
+        // set the value to the right of min index
+        for (let x = i + 1; x < anArray.length; x++) {
+            // if the value of the min value is greater than the value to the right of the min then...
+            if (anArray[min] > anArray[x]) {
+                // set min position to the position of the current element
+                min = x;
+            }
+        }
+        // swap the values at the min position and the fill slot
+        // set temporary value to min
+        let temp = anArray[i];
+        // this is the swap! the smaller value moves to the fill slot 
+        anArray[i] = anArray[min];
+        // new min value, this is the second part of the swap
+        anArray[min] = temp;
+    }
+}
+
+// Use Insertion Sort algorithm to sort array argument
+function insertionSort(anArray) {
+
+}
